81ec8a3b915d3265aa107e3e7c630f262405292a
Replit user <>
2023-08-15 20:32:01 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -13,7 +13,7 @@ int findMax(int arr[], int size) {
   
   for(int i=0;i<size-1;i++){
     for(int j=0;j<size-1-i;j++){
-      if(arr[j]>arr[j+1]){
+      if(arr[j]<arr[j+1]){
         int aux=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=aux;

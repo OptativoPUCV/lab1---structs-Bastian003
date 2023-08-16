8c70bb8ed56403bb6aceebcd5313a578ff1bb5e8
Replit user <>
2023-08-16 00:41:18 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -73,24 +73,19 @@ Descripción: Escribe una función que tome dos arreglos
 ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int *size3) {
-  *size3=size1+size2;
-  int *result=(int *)malloc(*size3 * sizeof(int));
-  int j=0;
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]) {
+  int size3=size1+size2;
+  int j=1;
   for(int i=0;i<size1;i++){
-    result[j]=arr1[i];
-    j++;
+    result[i]=arr1[i];
+    
   }
-
   for(int i=0;i<size2;i++){
-    result[j]=arr2[i];
-    j++;
+    result[size1+j]=arr2[i];
   }
-  for(int i=0;i<*size3;i++){
+  for(int i=0;i<size3;i++){
     printf("%i ",result[i]);
   }
-  
-  
 
   
 }

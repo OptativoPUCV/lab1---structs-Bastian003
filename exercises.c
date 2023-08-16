9f4dfdc7bf905194408797f1f24ecb5c47dff20e
Replit user <>
2023-08-16 00:06:24 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -57,8 +57,11 @@ int *filterEvenNumbers(int arr[], int size, int *newSize){
     }
   }
   nuevo=(int *)malloc(*newSize * sizeof(int));
-  for(int i=0;i<*newSize;i++){
-    nuevo[i]=arr[i];
+  for(int i=0;i<size;i++){
+    if(arr[i]%2==0){
+      nuevo[i]=arr[i];
+    }
+    
   }
   return nuevo; }
 

051af267e51dd34efd52846033d497e2af4023c7
Replit user <>
2023-08-16 00:24:52 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -73,8 +73,30 @@ Descripción: Escribe una función que tome dos arreglos
 ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]) {}
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int *size3) {
+  *size3=size1+size2;
+  for(int i=0;i<size1-1;i++){
+    for(int j=0;j<size1-1-i;j++){
+      if(arr1[j]<arr1[j+1]){
+        int aux=arr1[j];
+        arr1[j]=arr1[j+1];
+        arr1[j+1]=aux;
+      }
+    }
+  }
+  for(int i=0;i<size2-1;i++){
+    for(int j=0;j<size2-1-i;j++){
+      if(arr2[j]<arr2[j+1]){
+        int aux=arr2[j];
+        arr2[j]=arr2[j+1];
+        arr2[j+1]=aux;
+      }
+    }
+  }
+  
+
+  
+}
 
 /*
 Ejercicio 5: Comprobación de Ordenación

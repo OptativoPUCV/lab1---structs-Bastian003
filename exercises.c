2cd532823e454a54792e3f76eecb6203e7bfae86
Replit user <>
2023-08-17 01:38:26 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -155,6 +155,8 @@ typedef struct {
 void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,int anioNacimiento, int anioPublicacion) {
   strncpy(libro->titulo,titulo,sizeof(libro->titulo)-1);
   strncpy(libro->autor.nombre,nombreAutor,sizeof(libro->autor.nombre)-1);
+  libro->autor.anioNacimiento=anioNacimiento;
+  libro->anioPublicacion=anioPublicacion;
   
   
 }
